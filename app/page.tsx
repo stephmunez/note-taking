315936b490a6dba94282b24c619bcf96eb94b4c2
Stephen Muñez <sjtmunez@gmail.com>
2025-02-15 10:44:25 +0800

    feat: all notes empty state mobile

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,19 @@ export default function Home() {
   const currentTheme = theme === 'system' ? systemTheme : theme;
 
   return (
-    <main className='font-sans'>
-      <h1>Note Taking App</h1>
+    <main className='font-sans px-4 py-5'>
+      <div className='flex flex-col w-full gap-4'>
+        <h1 className='font-bold text-2xl leading-[1.2] tracking-[-0.5px]'>
+          All Notes
+        </h1>
+        <div className='flex flex-col gap-4 w-full'>
+          <p className='bg-neutral-100 border-neutral-200 rounded-lg p-2 text-sm leading-[1.3] tracking-[-0.2px] text-neutral-950 border-solid border dark:text-neutral-0 dark:bg-neutral-800 dark:border-neutral-700'>
+            You don’t have any notes yet. Start a new note to capture your
+            thoughts and ideas.
+          </p>
+        </div>
+      </div>
+
       <button
         onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}
         className='p-2 border rounded-md bg-gray-200 dark:bg-gray-800 dark:text-white'
