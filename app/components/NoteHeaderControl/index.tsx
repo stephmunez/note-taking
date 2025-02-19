2d1a08a209aecf340942d1398149dd949afc64e6
Stephen Muñez <sjtmunez@gmail.com>
2025-02-19 13:02:56 +0800

    fix: navigation from notes

--- app/components/NoteHeaderControl/index.tsx
+++ app/components/NoteHeaderControl/index.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import IconArchive from '../IconArchive';
 import IconArrowLeft from '../IconArrowLeft';
@@ -15,6 +16,15 @@ interface NoteHeaderControlProps {
 const NoteHeaderControl = ({ id, isArchived }: NoteHeaderControlProps) => {
   const { theme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    if (window.history.length > 2) {
+      router.back();
+    } else {
+      router.push(isArchived ? '/archive' : '/');
+    }
+  };
 
   useEffect(() => {
     setMounted(true);
@@ -27,10 +37,7 @@ const NoteHeaderControl = ({ id, isArchived }: NoteHeaderControlProps) => {
   const currentTheme = theme === 'system' ? systemTheme : theme;
   return (
     <div className="flex w-full items-center justify-between border-b border-solid border-neutral-200 pb-3 transition-colors duration-300 dark:border-neutral-800">
-      <Link
-        className="flex w-max items-center gap-1"
-        href={isArchived ? '/archive' : '/'}
-      >
+      <button className="flex w-max items-center gap-1" onClick={handleGoBack}>
         <span>
           <IconArrowLeft
             darkColor="#CACFD8"
@@ -43,7 +50,7 @@ const NoteHeaderControl = ({ id, isArchived }: NoteHeaderControlProps) => {
         <span className="text-sm leading-[1.3] tracking-[-0.2px] text-neutral-600 transition-colors duration-300 dark:text-neutral-300">
           Go Back
         </span>
-      </Link>
+      </button>
       <div className="flex items-center gap-4">
         <button>
           <IconDelete
