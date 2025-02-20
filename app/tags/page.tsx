ed75fbbb40f1d38f8dd766bdf666210d59bc4ec9
Stephen Muñez <sjtmunez@gmail.com>
2025-02-20 14:52:53 +0800

    fix: tags empty text transition

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
             ))}
           </ul>
         ) : (
-          <p className="rounded-lg border border-solid border-neutral-200 bg-neutral-100 p-2 text-sm leading-[1.3] tracking-[-0.2px] text-neutral-950 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-0">
+          <p className="rounded-lg border border-solid border-neutral-200 bg-neutral-100 p-2 text-sm leading-[1.3] tracking-[-0.2px] text-neutral-950 transition-colors duration-300 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-0">
             You don’t have any notes with tags yet. Start a new note to capture
             your thoughts and ideas.
           </p>
