63722fe4ed87d4c14f487e382066e7afe6139c31
Stephen Muñez <sjtmunez@gmail.com>
2025-02-24 12:14:00 +0800

    feat: login form

---
+++ app/components/LoginForm/index.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import IconHidePassword from '../IconHidePassword';
+import IconShowPassword from '../IconShowPassword';
+
+const LoginForm = () => {
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, [theme]);
+
+  if (!mounted) return null;
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+  return (
+    <form className="flex w-full flex-col gap-4 pt-6" onSubmit={handleSubmit}>
+      <div className="flex w-full flex-col gap-[0.375rem]">
+        <label
+          htmlFor="email"
+          className="text-sm font-medium leading-[1.2] tracking-[-0.2px] text-neutral-950 transition-colors duration-300 dark:text-neutral-0"
+        >
+          Email Address
+        </label>
+        <div className="relative">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full rounded-lg border border-solid border-neutral-300 bg-transparent px-4 py-3 pr-11 text-sm font-normal leading-[1.3] tracking-[-0.2px] text-neutral-950 transition-colors duration-300 placeholder:text-neutral-500 focus:outline-none dark:border-neutral-600 dark:text-neutral-0"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email@example.com"
+          />
+        </div>
+      </div>
+      <div className="flex w-full flex-col gap-[0.375rem]">
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="text-sm font-medium leading-[1.2] tracking-[-0.2px] text-neutral-950 transition-colors duration-300 dark:text-neutral-0"
+          >
+            Password
+          </label>
+          <Link
+            href={'/forgot-password'}
+            className="text-xs leading-[1.4] tracking-normal text-neutral-600 underline dark:text-neutral-400"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+
+        <div className="relative">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            name="password"
+            className="w-full rounded-lg border border-solid border-neutral-300 bg-transparent px-4 py-3 pr-11 text-sm font-normal leading-[1.3] tracking-[-0.2px] text-neutral-950 transition-colors duration-300 focus:outline-none dark:border-neutral-600 dark:text-neutral-0"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-[calc(50%-10px)]"
+            type="button"
+          >
+            {showPassword ? (
+              <IconHidePassword
+                theme={currentTheme}
+                width={20}
+                height={20}
+                lightColor="#717784"
+                darkColor="#717784"
+              />
+            ) : (
+              <IconShowPassword
+                theme={currentTheme}
+                width={20}
+                height={20}
+                lightColor="#717784"
+                darkColor="#717784"
+              />
+            )}
+          </button>
+        </div>
+      </div>
+      <button
+        className="rounded-lg bg-blue-500 px-4 py-3 text-sm font-medium leading-[1.2] tracking-[-0.2px] text-neutral-0 transition-colors duration-300"
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
