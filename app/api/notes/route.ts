e743e93d6797af62142e9424c9ad3bb62487c880
Stephen Muñez <sjtmunez@gmail.com>
2025-03-01 17:34:38 +0800

    feat: notes get request handler

---
+++ app/api/notes/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const res = await fetch('http://localhost:4000/notes');
+
+  const tickets = await res.json();
+
+  return NextResponse.json(tickets, {
+    status: 200,
+  });
+}
