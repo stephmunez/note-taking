10e10a76b0d7e2d98ba6b011b4525301d2a74577
Stephen Muñez <sjtmunez@gmail.com>
2025-03-04 11:38:50 +0800

    fix: create new note button

--- app/components/CreateNewNoteButton/index.tsx
+++ app/components/CreateNewNoteButton/index.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import iconPlus from '../../assets/images/icon-plus.svg';
 
 const CreateNewNoteButton = () => {
   return (
-    <button className='w-12 h-12 rounded-full bg-blue-500 flex items-center justify-center fixed bottom-[4.5rem] right-4'>
-      <Image src={iconPlus} width={32} height={32} alt='cross icon' />
-    </button>
+    <Link
+      href={'/create'}
+      className="fixed bottom-[4.5rem] right-4 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-blue-500"
+    >
+      <Image src={iconPlus} width={32} height={32} alt="cross icon" />
+    </Link>
   );
 };
 
