4cb4773fa9940d621b6089d2416b97583f9a222e
Stephen Muñez <sjtmunez@gmail.com>
2025-03-13 11:05:59 +0800

    feat: icon checkmark

---
+++ app/components/IconCheckmark/index.tsx
@@ -0,0 +1,25 @@
+interface IconCheckmarkProps {
+  width?: number;
+  height?: number;
+}
+
+const IconCheckmark = ({ width, height }: IconCheckmarkProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width ? width : 24}
+      height={height ? height : 24}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#21C16B"
+        fillRule="evenodd"
+        d="m15.993 10.222-4.618 4.618a.746.746 0 0 1-1.061 0l-2.309-2.309a.75.75 0 0 1 1.06-1.061l1.78 1.779 4.087-4.088a.75.75 0 1 1 1.061 1.061ZM12 2.5c-5.238 0-9.5 4.262-9.5 9.5 0 5.239 4.262 9.5 9.5 9.5s9.5-4.261 9.5-9.5c0-5.238-4.262-9.5-9.5-9.5Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default IconCheckmark;
