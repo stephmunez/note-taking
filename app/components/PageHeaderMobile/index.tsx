c58aab0ffc63a031af9419ba2e2921abc17259aa
Stephen Muñez <sjtmunez@gmail.com>
2025-03-21 17:48:03 +0800

    feat: page header tablet view

--- app/components/PageHeaderMobile/index.tsx
+++ app/components/PageHeaderMobile/index.tsx
@@ -2,7 +2,7 @@ import Logo from '../Logo';
 
 const PageHeaderMobile = () => {
   return (
-    <header className='px-4 py-3 w-full bg-neutral-100 dark:bg-neutral-800 transition-colors duration-300'>
+    <header className="w-full bg-neutral-100 px-4 py-3 transition-colors duration-300 dark:bg-neutral-800 md:px-8 md:py-6">
       <Logo />
     </header>
   );
