d9eeb0f4c3c16051d15562b5795cd360ca040910
Stephen Muñez <sjtmunez@gmail.com>
2025-03-22 17:48:03 +0800

    feat: home page tablet view

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   return (
-    <main className="z-50 min-h-[calc(100vh-108px)] rounded-t-lg bg-neutral-0 px-4 pb-16 pt-5 transition-colors duration-300 dark:bg-neutral-950">
+    <main className="z-50 min-h-[calc(100vh-108px)] rounded-t-lg bg-neutral-0 px-4 pb-16 pt-5 transition-colors duration-300 dark:bg-neutral-950 md:min-h-[calc(100vh-148px)] md:px-8 md:pt-6">
       <div className="flex w-full flex-col gap-4">
         <h1 className="text-2xl font-bold leading-[1.2] tracking-[-0.5px]">
           All Notes
