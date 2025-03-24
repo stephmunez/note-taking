57a281553f913da151826e2b79380427b1b491a2
Stephen Muñez <sjtmunez@gmail.com>
2025-03-24 13:43:48 +0800

    feat: note page tablet view

--- app/(dashboard)/notes/[id]/page.tsx
+++ app/(dashboard)/notes/[id]/page.tsx
@@ -31,7 +31,7 @@ const NotePage = async ({ params }: NotePageProps) => {
   const { id } = await params;
 
   return (
-    <main className="flex min-h-[calc(100vh-108px)] w-full flex-col gap-3 rounded-t-lg bg-neutral-0 px-4 py-5 dark:bg-neutral-950">
+    <main className="flex min-h-[calc(100vh-108px)] w-full flex-col gap-3 rounded-t-lg bg-neutral-0 px-4 py-5 dark:bg-neutral-950 md:min-h-[calc(100vh-148px)] md:gap-4 md:px-6">
       <NoteHeaderControl id={id} />
 
       <Suspense
