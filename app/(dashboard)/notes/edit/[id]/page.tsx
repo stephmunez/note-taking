2724a8643248d5ae0c6e6f30c79fc7f7bd621504
Stephen Muñez <sjtmunez@gmail.com>
2025-03-24 13:47:31 +0800

    feat: note edit page tablet view

--- app/(dashboard)/notes/edit/[id]/page.tsx
+++ app/(dashboard)/notes/edit/[id]/page.tsx
@@ -30,7 +30,7 @@ const EditNotePage = async ({ params }: EditNotePageProps) => {
   const { id } = await params;
 
   return (
-    <main className="flex min-h-[calc(100vh-108px)] w-full flex-col gap-3 rounded-t-lg bg-neutral-0 px-4 py-5 dark:bg-neutral-950">
+    <main className="dark:bg-neutral-95 flex min-h-[calc(100vh-108px)] w-full flex-col gap-3 rounded-t-lg bg-neutral-0 px-4 py-5 md:min-h-[calc(100vh-148px)] md:gap-4 md:px-6">
       <NoteHeaderControl id={id} isEdit={true} />
       <EditNote id={id} />
     </main>
