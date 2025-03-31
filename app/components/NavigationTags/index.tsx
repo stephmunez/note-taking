2c9e036f43d165ac0dcaab4b713a29e833451f68
Stephen Muñez <sjtmunez@gmail.com>
2025-03-31 17:44:49 +0800

    feat: navigation tags

---
+++ app/components/NavigationTags/index.tsx
@@ -0,0 +1,16 @@
+import NavigationTagsList from '../NavigationTagsList';
+
+const NavigationTags = () => {
+  return (
+    <div className="flex w-full flex-col gap-2 px-2">
+      <h2 className="text-sm font-medium leading-[1.2] tracking-[-0.2px] text-neutral-500">
+        Tags
+      </h2>
+      <nav>
+        <NavigationTagsList />
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationTags;
