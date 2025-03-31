ba1fa4e31357f67e8f99ab7bddddeb4d006acafb
Stephen Muñez <sjtmunez@gmail.com>
2025-03-31 17:45:07 +0800

    fix: tag item font weight

--- app/components/TagItem/index.tsx
+++ app/components/TagItem/index.tsx
@@ -26,7 +26,7 @@ const TagItem = ({ tag }: TagItemProps) => {
   return (
     <Link
       href={`/tags/${tag.toLowerCase()}`}
-      className="flex items-center gap-2 py-[0.625rem] text-sm leading-[1.2] tracking-[-0.2px] text-neutral-700 transition-colors duration-300 dark:text-neutral-300"
+      className="flex items-center gap-2 py-[0.625rem] text-sm font-medium leading-[1.2] tracking-[-0.2px] text-neutral-700 transition-colors duration-300 dark:text-neutral-300"
       key={tag}
     >
       <IconTag
