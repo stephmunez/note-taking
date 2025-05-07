9204317a98a2afedd8e442a27fe035a2abd68adc
Stephen Muñez <sjtmunez@gmail.com>
2025-05-07 19:53:03 +0800

    feat: settings pages width

--- app/(dashboard)/settings/password/page.tsx
+++ app/(dashboard)/settings/password/page.tsx
@@ -6,9 +6,9 @@ export const metadata: Metadata = {
   title: 'Notes Taking | Settings | Change Password',
 };
 
-const ColorTheme = () => {
+const ChangePassword = () => {
   return (
-    <main className="z-50 flex min-h-[calc(100vh-108px)] flex-col gap-5 rounded-t-lg bg-neutral-0 px-4 pb-16 pt-5 transition-colors duration-300 dark:bg-neutral-950 md:min-h-[calc(100vh-148px)] md:gap-6 md:px-8 md:py-6">
+    <main className="z-50 flex min-h-[calc(100vh-108px)] w-full flex-col gap-5 rounded-t-lg bg-neutral-0 px-4 pb-16 pt-5 transition-colors duration-300 dark:bg-neutral-950 md:min-h-[calc(100vh-148px)] md:gap-6 md:px-8 md:py-6 lg:max-w-[592px]">
       <div className="flex w-full flex-col gap-3">
         <SettingsHeaderControl />
         <h1 className="text-2xl font-bold leading-[1.2] tracking-[-0.5px] text-neutral-950 transition-colors duration-300 dark:text-neutral-0">
@@ -20,4 +20,4 @@ const ColorTheme = () => {
   );
 };
 
-export default ColorTheme;
+export default ChangePassword;
